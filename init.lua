e4e9c44d52a50f5be05880f104bec8cf0cc40eac
ajmandourah <ajmandourah@gmail.com>
2024-07-25 10:56:46 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -743,7 +743,7 @@ cmp.setup {
     ['<C-j>'] = cmp.mapping.scroll_docs(4),
     -- changing C-Space as it wont work in windows terminal
     ['<C-a>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<S-CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
